bddece4faa1f751d090b75022cb3d66c90af678c
supertigerzou <supertigerzou@gmail.com>
2015-03-08 17:26:16 +0800

    return raw message in wechat verification

--- src/TigerStudio.API/Modules/TigerStudio.Wechat/Controllers/WechatController.cs
+++ src/TigerStudio.API/Modules/TigerStudio.Wechat/Controllers/WechatController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -14,14 +15,17 @@ namespace TigerStudio.Wechat.Controllers
     {
         private const string Token = "alibaba";
 
+        // sample: api/wechat?signature=af30f7826dd7c71e23291dcb9ab94be53b7b87dd&timestamp=1425805977&nonce=739636270&echostr=hello
         [Route("")]
-        public IHttpActionResult Get(string signature, string timestamp, string nonce, string echostr)
+        public HttpResponseMessage Get(string signature, string timestamp, string nonce, string echostr)
         {
+            var stringToReturn = string.Empty;
+
             Trace.TraceInformation(string.Format("Signature: {0}, Timestamp: {1}, Nonce: {2}, Echostr: {3}", signature, timestamp, nonce, echostr));
-            if (CheckSignature(signature, timestamp, nonce)) 
-                return Ok(echostr);
-            
-            return BadRequest();
+            if (CheckSignature(signature, timestamp, nonce)) stringToReturn = echostr;
+
+            return new HttpResponseMessage()
+            { Content = new StringContent(stringToReturn, Encoding.UTF8, "text/html") };
         }
 
         private bool CheckSignature(string signature, string timestamp, string nonce)
@@ -31,7 +35,7 @@ namespace TigerStudio.Wechat.Controllers
 
             var tmpStr = string.Join("", arrTmp);
             tmpStr = FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1");
-            
+
             return tmpStr != null && tmpStr.ToLower() == signature;
         }
     }

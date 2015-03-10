6d6c6e1677845af0c612315e7400143f6d73c97c
supertigerzou <supertigerzou@gmail.com>
2015-03-10 15:06:24 +0800

    test menu

--- src/TigerStudio.API/Modules/TigerStudio.Wechat/Controllers/WechatController.cs
+++ src/TigerStudio.API/Modules/TigerStudio.Wechat/Controllers/WechatController.cs
@@ -28,8 +28,7 @@ namespace TigerStudio.Wechat.Controllers
             Trace.TraceInformation(string.Format("Signature: {0}, Timestamp: {1}, Nonce: {2}, Echostr: {3}", signature, timestamp, nonce, echostr));
             if (CheckSignature(signature, timestamp, nonce)) stringToReturn = echostr;
 
-            return new HttpResponseMessage()
-            { Content = new StringContent(stringToReturn, Encoding.UTF8, "text/html") };
+            return new HttpResponseMessage() { Content = new StringContent(stringToReturn, Encoding.UTF8, "text/html") };
         }
 
         // <xml><URL><![CDATA[http://tigerstudioapi.azurewebsites.net/api/wechat/message]]></URL><ToUserName><![CDATA[tigerartstudio]]></ToUserName><FromUserName><![CDATA[supertigerzou]]></FromUserName><CreateTime>1448841860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[test]]></Content><MsgId>1234367812123456</MsgId></xml>
@@ -41,31 +40,44 @@ namespace TigerStudio.Wechat.Controllers
             var inputMessageXml = reader.ReadToEnd();
             var inputMessage = Message.ConvertMessage(XElement.Parse(inputMessageXml));
 
-            StringContent replyContent;
-            if (inputMessage is ContentMessage && ((ContentMessage) inputMessage).Content.Contains("葫芦"))
-            {
-                replyContent = new StringContent(
-                    inputMessage.ReplyContent("请使用菜单收听，谢谢！").ToXml().ToString(), Encoding.UTF8, "xml/application");
-            }
-            else if (inputMessage is ContentMessage && ((ContentMessage) inputMessage).Content.Contains("图文测试"))
+            StringContent replyContent = null;
+            if (inputMessage is EventMessage)
             {
                 var mediaMessage = inputMessage.ReplyMedia(new List<ImageObject>
-                {
-                    new ImageObject()
                     {
-                        Description = "world",
-                        ImageUrl = "http://mmbiz.qpic.cn/mmbiz/u1YPNZkLJC6zYpt4jJJXRyYiaXMzrVyIFqeRicRUq4kmhzzAAjTIzYAp8EJ7BXbQ4ibicsv9sjr18QBbwkoAxO75GA/640?tp=webp&wxfrom=5",
-                        Title = "hello",
-                        Url =
-                            "http://mp.weixin.qq.com/s?__biz=MzA5NTU0MTMzOQ==&mid=204860621&idx=1&sn=ce5c822c25bc7526c36a4ca104bf04c8#rd"
-                    }
-                });
-
-                var testString = string.Format("<xml><ToUserName><![CDATA[{0}]]></ToUserName><FromUserName><![CDATA[{1}]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[news]]></MsgType><ArticleCount>1</ArticleCount><Articles><item><Title><![CDATA[title1]]></Title><Description><![CDATA[description1]]></Description><PicUrl><![CDATA[http://mmbiz.qpic.cn/mmbiz/u1YPNZkLJC6zYpt4jJJXRyYiaXMzrVyIFqeRicRUq4kmhzzAAjTIzYAp8EJ7BXbQ4ibicsv9sjr18QBbwkoAxO75GA/640?tp=webp&wxfrom=5]]></PicUrl><Url><![CDATA[http://mp.weixin.qq.com/s?__biz=MzA5NTU0MTMzOQ==&mid=204860621&idx=1&sn=ce5c822c25bc7526c36a4ca104bf04c8#rd]]></Url></item></Articles></xml>"
-                    , mediaMessage.ToUserName, mediaMessage.FromUserName);
-
+                        new ImageObject()
+                        {
+                            Description = "world",
+                            ImageUrl = "http://mmbiz.qpic.cn/mmbiz/u1YPNZkLJC6zYpt4jJJXRyYiaXMzrVyIFqeRicRUq4kmhzzAAjTIzYAp8EJ7BXbQ4ibicsv9sjr18QBbwkoAxO75GA/640?tp=webp&wxfrom=5",
+                            Title = "hello",
+                            Url =
+                                "http://mp.weixin.qq.com/s?__biz=MzA5NTU0MTMzOQ==&mid=204860621&idx=1&sn=ce5c822c25bc7526c36a4ca104bf04c8#rd"
+                        }
+                    });
 
                 replyContent = new StringContent(mediaMessage.ToXml().ToString(SaveOptions.DisableFormatting), Encoding.UTF8, "xml/application");
+            }
+            else if (inputMessage is ContentMessage)
+            {
+                if (((ContentMessage)inputMessage).Content.Contains("葫芦"))
+                    replyContent = new StringContent(
+                        inputMessage.ReplyContent("请使用菜单收听，谢谢！").ToXml().ToString(), Encoding.UTF8, "xml/application");
+                else if (((ContentMessage)inputMessage).Content.Contains("图文测试"))
+                {
+                    var mediaMessage = inputMessage.ReplyMedia(new List<ImageObject>
+                    {
+                        new ImageObject()
+                        {
+                            Description = "world",
+                            ImageUrl = "http://mmbiz.qpic.cn/mmbiz/u1YPNZkLJC6zYpt4jJJXRyYiaXMzrVyIFqeRicRUq4kmhzzAAjTIzYAp8EJ7BXbQ4ibicsv9sjr18QBbwkoAxO75GA/640?tp=webp&wxfrom=5",
+                            Title = "hello",
+                            Url =
+                                "http://mp.weixin.qq.com/s?__biz=MzA5NTU0MTMzOQ==&mid=204860621&idx=1&sn=ce5c822c25bc7526c36a4ca104bf04c8#rd"
+                        }
+                    });
+
+                    replyContent = new StringContent(mediaMessage.ToXml().ToString(SaveOptions.DisableFormatting), Encoding.UTF8, "xml/application");
+                }
             }
             else
             {
